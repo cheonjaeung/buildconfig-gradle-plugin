93c50297c56546a59053e841cde13e431c28363d
Jaewoong Cheon <cheonjaewoong@gmail.com>
2022-02-28 09:46:11 +0900

    Apply com.vanniktech.maven.publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-gradle-plugin")
     kotlin("jvm") version "1.6.10"
+    id("com.vanniktech.maven.publish") version "0.15.1"
     id("maven-publish") // For publish to maven local to test
 }
 
@@ -22,4 +23,10 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("gradle-plugin"))
     implementation("com.squareup:javapoet:1.13.0")
+}
+
+plugins.withId("com.vanniktech.maven.publish") {
+    mavenPublish {
+        sonatypeHost = com.vanniktech.maven.publish.SonatypeHost.S01
+    }
 }
